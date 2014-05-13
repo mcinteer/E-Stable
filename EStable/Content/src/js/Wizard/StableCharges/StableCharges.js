06a1c1dd38087746e4a10de8b7c9bfd85cf369ec
mcinteer <ryan.mcinteer@gmail.com>
2014-05-13 23:13:52 +1200

    Javascript the things

--- EStable/Content/src/js/Wizard/StableCharges/StableCharges.js
+++ EStable/Content/src/js/Wizard/StableCharges/StableCharges.js
@@ -1,120 +1,96 @@
-﻿var SITE = SITE || {};
+﻿$(function() {
 
-SITE.fileInputs = function () {
-    var $this = $(this),
-        $val = $this.val(),
-        valArray = $val.split('\\'),
-        newVal = valArray[valArray.length - 1],
-        $button = $this.siblings('.button'),
-        $fakeFile = $this.siblings('.file-holder');
-    if (newVal !== '') {
-        $button.removeClass('btn-info');
-        $button.addClass('btn-default');
-        $('#confirmImportButton').removeClass('dont-display');
-        $('#importStableChargeFilePath').val = newVal;
-        if ($fakeFile.length === 0) {
-            $button.after('<span class="file-holder">' + newVal + '</span>');
+    var StableCharges = {
+        
+        createStableChargesTable : function(data) {
+            $('#tblStableCharges').dynatable({
+                dataset: {
+                    records: data
+                },
+                table: {
+                    defaultColumnIdStyle: 'asis'
+                },
+                inputs: {
+                    processingText: 'Fetching new Charges'
+                }
+            }); 
+        },
+        
+        deselectStableCharge: function() {
+            $(".pop-stable-charge").slideFadeToggle(function () {
+                $("#addStableCharge").removeClass("selected");
+            });    
+        }
+        
+    };
+
+    function htmlEncode(value) {
+        if (value) {
+            return jQuery('<div />').text(value).html();
         } else {
-            $fakeFile.text(newVal);
+            return '';
         }
     }
-};
 
-SITE.confirmImport = function() {
-    var dynatable = $('#tblStableCharges').data('dynatable');
-
-    dynatable.processingIndicator.hide();
-    dynatable.processingIndicator.show();
-
-    $.ajax({
-        type: "POST",
-        url: '../../../../../Wizard/ImportStableCharges',
-        data: {
-            path: $('#importStableChargeFilePath').val(),
-            email: $('#email').val()
-        },
-        success: function(data) {
-            dynatable.records.updateFromJson(data);
-            dynatable.settings.dataset.originalRecords = dynatable.settings.dataset.records
-            dynatable.process();
-            dynatable.processingIndicator.hide();
-        },
-        error: function(data) {
-        }
-    });
-};
-
-$(document).ready(function () {
-    $('.file-wrapper input[type=file]')
-        .bind('change focus click', SITE.fileInputs);
-
-    $('#confirmImportButton')
-        .bind('click', SITE.confirmImport);
-});
-
-function createStableChargesTable(data) {
-    $('#tblStableCharges').dynatable({
-        dataset: {
-            records: decode(data)
-        },
-        table: {
-            defaultColumnIdStyle: 'asis'
-        },
-        inputs: {
-            processingText: 'Fetching new Charges'
-        }
-    }); 
-}
-
-function deselectStableCharge() {
-    $(".pop-stable-charge").slideFadeToggle(function () {
-        $("#addStableCharge").removeClass("selected");
-    });    
-}
-
-$(function() {
-    $("#addStableCharge").live('click', function() {
-        if($(this).hasClass("selected")) {
-            deselectStableCharge();               
+    function htmlDecode(value) {
+        if (value) {
+            return $('<div />').html(value).text();
         } else {
-            $(this).addClass("selected");
-            $(".pop-stable-charge").slideFadeToggle(function() { 
-                $("#stbl-unit").focus();
-            });
+            return '';
         }
-        return false;
-    });
+    }
 
-    $("#close-stable-charge").live('click', function () {
-        deselectStableCharge();
-        return false;
-    });
-});
-
-$(function() {
-    $('#submit-add-stable-charge').live('click', function() {
-        var dynatable = $('#tblStableCharges').data('dynatable');
-
-        dynatable.processingIndicator.hide();
-        dynatable.processingIndicator.show();
-        $.ajax({
-            type: "POST",
-            url: '../../../../../Wizard/SaveStableCharge',
-            data: {
-                unit : $('#stbl-unit').val(),
-                instable: $('#instable').val(),
-                description: $('#stbl-description').val(),
-                rate: $('#stbl-rate').val(),
-                email: $('#email').val()
-            },
-            success: function (data) {
-                dynatable.records.updateFromJson(data);
-                dynatable.settings.dataset.originalRecords = dynatable.settings.dataset.records
-                dynatable.process();
-                dynatable.processingIndicator.hide();
-            },
-            error : function(data) {
+    function setupEventListeners() {
+        $("#addStableCharge").live('click', function () {
+            if ($(this).hasClass("selected")) {
+                deselectStableCharge();
+            } else {
+                $(this).addClass("selected");
+                $(".pop-stable-charge").slideFadeToggle(function () {
+                    $("#stbl-unit").focus();
+                });
             }
-        }); 
-    });
+            return false;
+        });
+
+        $("#close-stable-charge").live('click', function () {
+            deselectStableCharge();
+            return false;
+        });
+
+        $('#submit-add-stable-charge').live('click', function () {
+            var dynatable = $('#tblStableCharges').data('dynatable');
+
+            dynatable.processingIndicator.hide();
+            dynatable.processingIndicator.show();
+            $.ajax({
+                type: "POST",
+                url: '../../../../../Wizard/SaveStableCharge',
+                data: {
+                    unit: $('#stbl-unit').val(),
+                    instable: $('#instable').val(),
+                    description: $('#stbl-description').val(),
+                    rate: $('#stbl-rate').val(),
+                    email: $('#email').val()
+                },
+                success: function (data) {
+                    dynatable.records.updateFromJson(data);
+                    dynatable.settings.dataset.originalRecords = dynatable.settings.dataset.records
+                    dynatable.process();
+                    dynatable.processingIndicator.hide();
+                },
+                error: function (data) {
+                }
+            });
+        });
+    }
+    
+    if (!$('#data').length) {
+        console.warn('You need to define your data Ryan!');
+        return;
+    }
+
+    setupEventListeners();
+    var pageData = htmlDecode($('#data').html());
+    StableCharges.createStableChargesTable(JSON.parse(pageData));
 });
