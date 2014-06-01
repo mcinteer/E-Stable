4acaf351b8489e700b70e26cc160fb43bd71a2ac
mcinteer <ryan.mcinteer@gmail.com>
2014-06-02 08:36:54 +1200

    get mysql properly

--- EStable/Constants/Codes.cs
+++ EStable/Constants/Codes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using MySql.Data.MySqlClient;
-
-namespace EStable.Constants
+﻿namespace EStable.Constants
 {
     public static class Codes
     {
