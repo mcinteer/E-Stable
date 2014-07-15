04b523baa6739e6ef272f53fc5fcebeab4358541
mcinteer <ryan.mcinteer@gmail.com>
2014-07-15 21:38:24 +1200

    slickgrid

--- EStable/Content/dist/js/EstableBase.js
+++ EStable/Content/dist/js/EstableBase.js
@@ -10890,6 +10890,69 @@ function setupSubmitAddOwnerClickEvent(tableID) {
     stableCharges.createStableChargesTable(JSON.parse(stableChargeData));
     setupStableChargeInputs();
 });
+﻿$(function () {
+	var stableCharges = {
+		columns: [
+			{
+				name: 'Unit',
+				field: 'Unit',
+				id: 'unit',
+				editor: Slick.Editors.Text
+			},
+			{
+				name: 'In Stable',
+				field:'InStable',
+				id:'in-stable',
+				editor: Slick.Editors.Text
+			},
+			{
+				name: 'Description',
+				field: 'Description',
+				id: 'description',
+				editor: Slick.Editors.Text
+			},
+			{
+				name: 'Rate',
+				field: 'Rate',
+				id: 'rate',
+				editor: Slick.Editors.Text
+			}
+		],
+		rows: [
+			{
+				Unit: 'unit',
+				InStable: 'in-stable',
+				Description: 'description',
+				Rate: 'rate'
+			},
+			{
+				Unit: 'unit',
+				InStable: 'in-stable',
+				Description: 'description',
+				Rate: 'rate'
+			},
+			{
+				Unit: 'unit',
+				InStable: 'in-stable',
+				Description: 'description',
+				Rate: 'rate'
+			}
+		],
+		options: {
+				editable: true,
+				enableAddRow: false,
+				enableCellNavigation: true,
+				asyncEditorLoading: false
+			},
+		controller: {
+			Initialize: function() {
+				grid = new Slick.Grid('#slick-example', stableCharges.rows, stableCharges.columns, stableCharges.options);
+			}
+		}
+	};
+
+	stableCharges.controller.Initialize();
+});
 ﻿$(function() {
     var standardCharges = {
         createStandardChargesTable: function(data) {
---
+++ EStable/Content/src/js/Wizard/StableCharges/StableChargesWithSlickGrid.js
@@ -0,0 +1,63 @@
+﻿$(function () {
+	var stableCharges = {
+		columns: [
+			{
+				name: 'Unit',
+				field: 'Unit',
+				id: 'unit',
+				editor: Slick.Editors.Text
+			},
+			{
+				name: 'In Stable',
+				field:'InStable',
+				id:'in-stable',
+				editor: Slick.Editors.Text
+			},
+			{
+				name: 'Description',
+				field: 'Description',
+				id: 'description',
+				editor: Slick.Editors.Text
+			},
+			{
+				name: 'Rate',
+				field: 'Rate',
+				id: 'rate',
+				editor: Slick.Editors.Text
+			}
+		],
+		rows: [
+			{
+				Unit: 'unit',
+				InStable: 'in-stable',
+				Description: 'description',
+				Rate: 'rate'
+			},
+			{
+				Unit: 'unit',
+				InStable: 'in-stable',
+				Description: 'description',
+				Rate: 'rate'
+			},
+			{
+				Unit: 'unit',
+				InStable: 'in-stable',
+				Description: 'description',
+				Rate: 'rate'
+			}
+		],
+		options: {
+				editable: true,
+				enableAddRow: false,
+				enableCellNavigation: true,
+				asyncEditorLoading: false
+			},
+		controller: {
+			Initialize: function() {
+				grid = new Slick.Grid('#slick-example', stableCharges.rows, stableCharges.columns, stableCharges.options);
+			}
+		}
+	};
+
+	stableCharges.controller.Initialize();
+});
